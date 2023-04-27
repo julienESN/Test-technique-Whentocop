a6b0d5ccdb784c0ad5aba70c6c959b5255a2c7a6
Julien <julien.esnau@gmail.com>
2023-04-27 17:45:37 +0200

    Ajouter les fichiers du test technique et convertir App.js en App.tsx pour utiliser TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { drops_data } from './data/data.js';
+
+function App() {
+  return (
+    <div className="App">
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
